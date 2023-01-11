f66df886dad1236e2e9c2bee3687138a4a1808dc
NestorPala <npalavecino@fi.uba.ar>
2023-01-11 20:45:50 -0300

    add note form working

--- frontend/src/NotesList/AddNoteForm.jsx
+++ frontend/src/NotesList/AddNoteForm.jsx
@@ -1,6 +1,23 @@
 export function AddNoteForm(props) {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const url = props.url + "/create";
+        const options = {
+            method: "POST",
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: event.target.title.value,
+                content: event.target.content.value
+            })
+        };
+        fetch(url, options)
+            .then(res => res.json())
+            .then(json => console.log(json));
+    };
     return (
-        <form method="POST" action={props.url + "/create"}>
+        <form onSubmit={handleSubmit}>
             <input name="title" type="text" placeholder="Note title"/>
             <input name="content" type="text" placeholder="Note content"/>
             <button type="submit">Add note!</button>
