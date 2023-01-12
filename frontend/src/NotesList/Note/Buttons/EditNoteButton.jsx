1c4e554e72a5036595a6664684d008372795c858
NestorPala <npalavecino@fi.uba.ar>
2023-01-12 10:38:41 -0300

    edit note button working

--- frontend/src/NotesList/Note/Buttons/EditNoteButton.jsx
+++ frontend/src/NotesList/Note/Buttons/EditNoteButton.jsx
@@ -1,7 +1,42 @@
 import { StyledButton } from "./Buttons";
 
-export function EditNoteButton() {
+function editNote(event, props) {
+    event.preventDefault();
+    const newTitle = prompt("New title: ");
+    
+    if (newTitle === "" || newTitle == null) {
+        alert("Title cannot be empty");
+        return;
+    }
+
+    const newContent = prompt("New content: ");
+
+    if (newContent === "" || newContent == null) {
+        if (window.confirm("Sure you want to erase content?") === false) {
+            return;
+        }
+    }
+
+    const url = props.url + "/edit/" + props.note.id;
+    const options = {
+        method: "PATCH",
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: newTitle,
+            content: newContent
+        })
+    };
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => props.onEditNote(json));
+};
+
+export function EditNoteButton(props) {
     return (
-        <StyledButton>Edit</StyledButton>
+        <StyledButton onClick={e => editNote(e, props)}>
+            Edit
+        </StyledButton>
     );
 }
